57c724ce29259fefaa8d61cd3ac30a83c27226dc
Taisei Mima <22282293+bhbs@users.noreply.github.com>
2026-06-17 17:09:21 -0400

    Update Floodlight cask to 0.1.4

--- Casks/floodlight.rb
+++ Casks/floodlight.rb
@@ -1,6 +1,6 @@
 cask "floodlight" do
-  version "0.1.3"
-  sha256 "dfa838b2207be63b54cad9e7badc6415dd2d235786d9006ee92e69af56771b33"
+  version "0.1.4"
+  sha256 "2660e5bb2bcf7948f0112831de1e8879e057952d1fc4da4ce608e56c805fccf1"
 
   url "https://github.com/bhbs/floodlight/releases/download/v#{version}/Floodlight-#{version}-notarized.zip"
   name "Floodlight"
