41b3bf539b463380caa2f71be784db9790b8e01a
Taisei Mima <22282293+bhbs@users.noreply.github.com>
2026-07-10 04:16:19 -0400

    Update FlatWheel cask to 0.1.8

--- Casks/flatwheel.rb
+++ Casks/flatwheel.rb
@@ -1,6 +1,6 @@
 cask "flatwheel" do
-  version "0.1.2"
-  sha256 "5b81c54f5fb60774359905f4b90638751aa35bdab2b65ed2743cd762d1d9e803"
+  version "0.1.8"
+  sha256 "6174221c70a2fea908a6656918ad109fdd167028f1fb3294a68613ea6f0117e8"
 
   url "https://github.com/bhbs/flatwheel/releases/download/v#{version}/FlatWheel-#{version}-notarized.zip"
   name "FlatWheel"
