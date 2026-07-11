197976045bef22928adbf5b8c7f206381f769019
Taisei Mima <22282293+bhbs@users.noreply.github.com>
2026-07-11 17:55:40 -0400

    Update RabbitDictionary cask to 0.1.8

--- Casks/rabbit-dictionary.rb
+++ Casks/rabbit-dictionary.rb
@@ -1,6 +1,6 @@
 cask "rabbit-dictionary" do
-  version "0.1.7"
-  sha256 "3d696b6f1a503ec5975191d70888578a4e5a877e9f8c53ed7ca87428863cd098"
+  version "0.1.8"
+  sha256 "04b5ca96a220003d7cb9316f097275f5e302f4cc9b023e62590c672e54b3e002"
 
   url "https://github.com/bhbs/rabbit-dictionary/releases/download/v#{version}/RabbitDictionary-#{version}-notarized.zip"
   name "RabbitDictionary"
