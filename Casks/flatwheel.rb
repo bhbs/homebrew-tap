b1d81820f0a10ebc99f0758d3c905a315e238a2e
Taisei Mima <22282293+bhbs@users.noreply.github.com>
2026-07-12 02:40:45 -0400

    Update FlatWheel cask to 0.1.9

--- Casks/flatwheel.rb
+++ Casks/flatwheel.rb
@@ -1,6 +1,6 @@
 cask "flatwheel" do
   version "0.1.9"
-  sha256 "0933229f2554a309756930f4825d1e5f40ac3ee75f0a531913a5947f911ab977"
+  sha256 "d3d574992454c3f3bbe78a6bb4a61605a238cc273e498117028ae111cbe18832"
 
   url "https://github.com/bhbs/flatwheel/releases/download/v#{version}/FlatWheel-#{version}-notarized.zip"
   name "FlatWheel"
@@ -9,7 +9,7 @@ cask "flatwheel" do
 
   depends_on macos: :sonoma
 
-  app "FlatWheel.app"
+  app "Flat Wheel.app"
 
   zap trash: [
     "~/Library/Logs/FlatWheel",
